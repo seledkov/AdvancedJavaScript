78203e52ae9797a796b63dec97f4ce536df204ca
unknown <seledkovti@gmail.com>
2022-04-03 17:31:42 +0300

    basic async work example

--- Lessions/17_AsyncJS_Promise_Callbacks/intro.js
+++ Lessions/17_AsyncJS_Promise_Callbacks/intro.js
@@ -1,7 +1,46 @@
 const btn = document.querySelector('button');
 
-function handleButtonClick() {
-  console.log('clicked');
+btn.style.color = 'blue';
+
+function trackUserHandler() {
+  // getCurr get success CB, error CB, options
+  navigator.geolocation.getCurrentPosition(
+    (dataLocation) => {
+      setTimeout(() => {
+        console.log('data location cb after 2s', dataLocation);
+      }, 2000);
+      console.log('data location cb', dataLocation);
+    },
+    (error) => {
+      console.log(error);
+    },
+  );
+  setTimeout(() => {
+    console.log('timer 0s');
+  }, 0);
+  console.log('getting position');
 }
 
-btn.addEventListener('click', handleButtonClick);
+btn.addEventListener('click', trackUserHandler);
+
+let result = 0;
+// ES2021 numberic separator
+for (let i = 0; i < 100_000_000; i++) {
+  result += i;
+}
+console.log(result);
+
+for (let i = 0; i < 100_000_000; i++) {
+  result += i;
+}
+console.log(result);
+
+for (let i = 0; i < 100_000_000; i++) {
+  result += i;
+}
+console.log(result);
+
+for (let i = 0; i < 100_000_000; i++) {
+  result += i;
+}
+console.log(result);
