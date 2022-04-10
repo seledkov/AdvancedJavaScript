f5bd6d82930793b654c33f28388e259b1d8d76ee
unknown <seledkovti@gmail.com>
2022-04-11 02:29:58 +0300

    lession 2 basics, varibales, string, number

--- Lessions/02_Basics/01/assets/scripts/app.js
+++ Lessions/02_Basics/01/assets/scripts/app.js
@@ -1,1 +1,8 @@
-alert('d');
+const defaultValue = 0;
+const calculationDescription = '(' + defaultValue + ' + 4) * 3 / 2 - 5 ** 2';
+
+let currentResult = defaultValue;
+
+currentResult = (((defaultValue + 8) * 3) / 2 - 5) ** 2;
+
+outputResult(currentResult, calculationDescription);
