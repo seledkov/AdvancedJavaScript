46353f48e36e5ad14bcb8b84beecca139cb77b87
unknown <seledkovti@gmail.com>
2022-04-19 22:56:38 +0300

    add good example

--- Lessions/02_Basics/01/assets/scripts/app.js
+++ Lessions/02_Basics/01/assets/scripts/app.js
@@ -1,8 +1,44 @@
 const defaultValue = 0;
 const calculationDescription = '(' + defaultValue + ' + 4) * 3 / 2 - 5 ** 2';
+let currentResult;
 
-let currentResult = defaultValue;
-
-currentResult = (((defaultValue + 8) * 3) / 2 - 5) ** 2;
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+currentResult = add(2, 4);
 
 outputResult(currentResult, calculationDescription);
+
+async function foo(callback) {
+  setTimeout(() => {
+    callback('A');
+  }, Math.random() * 1000);
+}
+const bar = async (callback) => {
+  setTimeout(() => {
+    callback('B');
+  }, Math.random() * 1000);
+};
+
+const baz = async (callback) => {
+  setTimeout(() => {
+    callback('C');
+  }, Math.random() * 1000);
+};
+
+const first = (func) => {
+  return new Promise((resolve) => {
+    func(resolve);
+  });
+};
+
+const final = async () => {
+  let a = await first(foo);
+  console.log(a);
+  let b = await first(bar);
+  console.log(b);
+  let c = await first(baz);
+  console.log(c);
+};
+final();
