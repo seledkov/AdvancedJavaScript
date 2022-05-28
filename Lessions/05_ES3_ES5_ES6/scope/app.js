32fbf5b84f419297fe44ecb89f6b19ee63f0467c
unknown <seledkovti@gmail.com>
2022-05-29 01:40:16 +0300

    var, hoisting

--- Lessions/05_ES3_ES5_ES6/scope/app.js
+++ Lessions/05_ES3_ES5_ES6/scope/app.js
@@ -1,16 +1,51 @@
-let name = 'Max';
+var name = 'Max';
 
 if (name === 'Max') {
+  // create as global variable
+  // var hobbies = ['Sports', 'Cooking'];
+  // create as block variable only into if scope
   let hobbies = ['Sports', 'Cooking'];
   console.log(hobbies);
 }
 
 function greet() {
-  let age = 30;
-  let name = 'Manuel';
-  console.log(name, age, hobbies);
+  var age = 30;
+  var name = 'Manuel';
+  console.log(name, age);
+  // console.log(name, age, hobbies);
 }
 
-console.log(name, hobbies);
+{
+  // create as block scope -  { } // if, for, foo, try/catch,  empty {}  xD
+  let blockA = 4;
+}
+
+// console.log(blockA);
+
+// console.log(name, hobbies);
 
 greet();
+
+// ==== var job ====
+// for (var i = 0; i < 4; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   });
+// }
+
+for (let i = 0; i < 4; i++) {
+  setTimeout(() => console.log(i));
+}
+
+// for (var i = 0; i < 4; i++) {
+//   console.log(i);
+// }
+
+// secondName hoisting and assign undefined
+console.log(secondName);
+
+var secondName = 'second';
+
+var a;
+// js engine clear next var and assign 5 to a
+var a = 5;
