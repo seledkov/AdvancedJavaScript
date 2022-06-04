ff5ef67f9b09cd55d8cacf2db74e6f35ebb9ba28
unknown <seledkovti@gmail.com>
2022-06-05 02:17:11 +0300

    add basic game logic

--- Lessions/06_Functions/RockPaperScissorsGame/app.js
+++ Lessions/06_Functions/RockPaperScissorsGame/app.js
@@ -1,18 +1,96 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-const foo = function foo2() {
-  console.log('foo', foo.name, new Error('err'));
-  // console.log('foo', foo.name);
+const selection = {
+  ROCK: 'ROCK',
+  PAPER: 'PAPER',
+  SCISSORS: 'SCISSORS',
+  DEFAULT_VALUE: 'ROCK',
 };
+
+const result = {
+  DRAW: 'DRAW',
+  PLAYER_WIN: 'PLAYER WIN',
+  COMPUTER_WIN: 'COMPUTER WIN',
+};
+
+let gameRuningStatus = false;
+
+const getPlayerChoice = () => {
+  let playerSelect = prompt(
+    `Select ${selection.ROCK},${selection.PAPER} or ${selection.SCISSORS}`,
+    '',
+  ).toUpperCase();
+
+  if (
+    playerSelect !== selection.ROCK &&
+    playerSelect !== selection.PAPER &&
+    playerSelect !== selection.SCISSORS
+  ) {
+    alert(`${playerSelect} incorrect select, we install you choice to ${selection.DEFAULT_VALUE}`);
+    playerSelect = selection.DEFAULT_VALUE;
+  }
+  return playerSelect;
+};
+
+const getComputerChoise = () => {
+  const randomValue = Math.random();
+  let computerSelect;
+  if (randomValue < 0.34) {
+    computerSelect = selection.PAPER;
+  } else if (randomValue > 0.33 && randomValue < 0.67) {
+    computerSelect = selection.ROCK;
+  } else {
+    computerSelect = selection.SCISSORS;
+  }
+  return computerSelect;
+};
+
+const getGameResult = (computerChoice, playerChoice) => {
+  let gameResult;
+
+  if (computerChoice === playerChoice) {
+    gameResult = result.DRAW;
+  } else if (
+    (computerChoice === selection.ROCK && playerChoice === selection.PAPER) ||
+    (computerChoice === selection.SCISSORS && playerChoice === selection.ROCK) ||
+    (computerChoice === selection.PAPER && playerChoice === selection.SCISSORS)
+  ) {
+    gameResult = result.PLAYER_WIN;
+  } else {
+    gameResult = result.COMPUTER_WIN;
+  }
+
+  return gameResult;
+};
+
+const startGameHandler = () => {
+  if (gameRuningStatus) {
+    return;
+  }
+
+  gameRuningStatus = true;
+
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoise();
+  const gameResult = getGameResult(computerChoice, playerChoice);
+  console.log(playerChoice, computerChoice, gameResult);
+
+  gameRuningStatus = false;
+};
+
+// const foo = function foo2() {
+//   console.log('foo', foo.name, new Error('err'));
+//   // console.log('foo', foo.name);
+// };
 
 // console.log(foo.name); // foo2
 // we can create name but it does not need and name returned after error
 // foo2(); // err
 
-const anonFoo = function () {
-  console.log('anonFoo', anonFoo.name);
-};
+// const anonFoo = function () {
+//   console.log('anonFoo', anonFoo.name);
+// };
 
-anonFoo(); // anonFoo
+// anonFoo(); // anonFoo
 
-startGameBtn.addEventListener('click', foo);
+startGameBtn.addEventListener('click', startGameHandler);
