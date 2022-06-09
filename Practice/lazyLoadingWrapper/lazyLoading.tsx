a6dd82e38e485a3c03961667ac947d96f3292f7a
unknown <seledkovti@gmail.com>
2022-06-10 01:35:47 +0300

    fix github place

--- Practice/lazyLoadingWrapper/lazyLoading.tsx
+++ Practice/lazyLoadingWrapper/lazyLoading.tsx
@@ -1,46 +1,57 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
-}
+export const useLazyScrollingWrapper = (
+  refObserverContainer: React.MutableRefObject<HTMLElement>,
+  refItemsContainer: React.MutableRefObject<HTMLElement>, // but ref === null in started
+  callback, // cb need promise?
+  deps, // need add data or cb result, this trigger update anchor element
+) => {
+  // todo - need update and request page logic into hook or components
+  // const [dataPage, setDataPage] = useState<number>(0);
+  //
+  // const fetchNewData = () => {
+  //   callback()
+  //   // getData({ page: dataPage });
+  //   setDataPage((prev) => prev + 1);
+  // };
 
-sayHello();
+  useEffect(() => {
+    const itemsContainer = refItemsContainer.current;
+    const observerContainer = refObserverContainer.current;
+    console.log('TS - container', itemsContainer?.children);
 
-// Re-write the function you find in <code>assignment.js</code> as an arrow function.
-
-// Перепишите функцию, найденную в <code>assignment.js</code>, как функцию стрелки.
-const sayHi = (name) => {
-  console.log('Hi ' + name);
-};
-
-// Adjust the arrow function you created as part of task 1 to use three different syntaxes: With two arguments (incl. a phrase that replaces "Hi"),
-// with no arguments (hard-coded values in function body) and with one returned value (instead of outputting text inside of the function directly).
-const sayHi1 = (msg, name) => {
-  console.log(`${msg} ${name}`);
-};
-const sayHi2 = () => {
-  console.log('Hi Max');
-};
-const sayHi3 = () => {
-  return 'Hi Max';
-};
-
-// Add a default argument to the function you created: A fallback value for the phrase if no value is provided.
-const sayHi4 = (name = 'Gerald') => {
-  console.log('Hi ' + name);
-};
-
-// Add a new function: "checkInput" which takes an unlimited amount of arguments (unlimited amount of strings) and executes a callback function
-// if NO argument/ string is an empty string.
-const checkInput = (cb, ...strings) => {
-  let hasEmptyString = false;
-
-  for (const string of strings) {
-    if (!string) {
-      hasEmptyString = true;
-      break;
+    if (itemsContainer?.children.length === 0) {
+      return;
     }
-  }
+    console.log('TS - call useLazy');
 
-  if (hasEmptyString) {
-    cb();
-  }
+    const options = { root: observerContainer, threshold: 0.9 };
+    const anchorElement: Element = itemsContainer.lastElementChild;
+
+    const observer = new IntersectionObserver((elements, observer) => {
+      elements.forEach((element) => {
+        console.log('TS - element ', element);
+        if (element.isIntersecting === true) {
+          console.log('TS - entry called backend');
+
+          callback();
+
+          // unobserve after callback end, and need new observe - hook reused if change data into deps
+          observer.unobserve(anchorElement);
+          console.log('TS - unobserve');
+        }
+      });
+    }, options);
+    // const observer = new IntersectionObserver(()=>console.log('hi'), options)
+
+    observer.observe(anchorElement);
+    console.log('TS - observe start');
+
+    return () => {
+      observer.unobserve(anchorElement);
+      console.log('TS - observe end');
+    };
+  }, [
+    // ref.current,
+    // refContainer.current,
+    deps.length,
+  ]);
 };
