714630b0e20032a0705885fe2acb760b6e042550
unknown <seledkovti@gmail.com>
2022-06-10 01:30:39 +0300

    add lazy loading React

---
+++ Lessions/08_Arrays_Iterables/arr/app.js
@@ -0,0 +1,8 @@
+const arr1 = [1, 2, 3];
+
+const arr2 = new Array(4); // empty arr witch length 4 [empty x4] // undefinded x4
+arr2[2] = 4;
+arr2.push(10);
+const arr3 = Array.of(11); // [11]
+
+const arr4 = Array.from('hi'); // ['h', 'i']
---
+++ Practice/lazyLoadingWrapper/app.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import { useLazyScrollingWrapper } from './assignment';
+
+const $Container = styled.div`
+  border: 1px solid red;
+  height: 400px;
+  width: 500px;
+  overflow: scroll;
+  padding: 10px;
+
+  div > div {
+    height: 270px;
+    width: 400px;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    border: 1px solid grey;
+    margin-bottom: 10px;
+  }
+`;
+
+const Page: React.FC<any> = () => {
+  const [arr, setArr] = useState<any>([]);
+
+  useEffect(() => {
+    setTimeout(() => setArr([1, 2, 3, 4]), 3000);
+  }, []);
+
+  const ref = useRef(null);
+  const refWrapper = useRef(null);
+
+  const cb = () => {
+    console.log('TS - backend get data and push in array');
+    setArr((prev) => [...prev, Math.random(), Math.random(), Math.random()]);
+  };
+
+  useLazyScrollingWrapper(refWrapper, ref, cb, arr);
+
+  return (
+    <$Container ref={refWrapper}>
+      <div ref={ref}>
+        {arr.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </div>
+    </$Container>
+  );
+};
+
+export default Page;
